c6cd8afe6475d50fb908108752e0091f63434d7b
rokinsky <ankezy@gmail.com>
2019-04-02 01:32:16 +0200

    skeleton works

--- usr/src/minix/servers/pm/distort_time.c
+++ usr/src/minix/servers/pm/distort_time.c
@@ -4,10 +4,10 @@
 #include <sys/types.h>
 
 int do_distort_time() {
-  int caller = m_in.m1_i1;
-  int contender = m_in.m1_i2;
-  int scale = m_in.m1_i3;
+  pid_t caller = m_in.m1_i1;
+  pid_t contender = m_in.m1_i2;
+  uint8_t scale = m_in.m1_i3;
 
-  printf("Hello world!\n");
-  return 0; // OK;
+  printf("distort_time: caller(%d), (%d, %d).\n", caller, contender, scale);
+  return OK;
 }
