8991236c2aa867555fcfe8297ab663eb73720678
rokinsky <ankezy@gmail.com>
2019-04-01 23:54:09 +0200

    reverted to void

--- usr/src/minix/servers/pm/distort_time.c
+++ usr/src/minix/servers/pm/distort_time.c
@@ -1,6 +1,6 @@
 #include "proto.h"
 
-int do_distort_time(message *m) {
+int do_distort_time() {
   printf("Hello world!\n");
   return OK;
 }
--- usr/src/minix/servers/pm/proto.h
+++ usr/src/minix/servers/pm/proto.h
@@ -91,4 +91,4 @@ int pm_isokendpt(int ep, int *proc);
 void tell_vfs(struct mproc *rmp, message *m_ptr);
 
 /* distort_time.c */
-int do_distort_time(message *);
+int do_distort_time(void);
