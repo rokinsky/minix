a7769ae178838bca3c806942cda55dea88ea85d8
rokinsky <ankezy@gmail.com>
2019-04-02 01:09:44 +0200

    types combination

--- usr/src/minix/servers/pm/distort_time.c
+++ usr/src/minix/servers/pm/distort_time.c
@@ -4,9 +4,9 @@
 #include <sys/types.h>
 
 int do_distort_time() {
-  pid_t caller = m_in.m1_i1;
-  pid_t contender = m_in.m1_i2;
-  uint8_t scale = m_in.m1_i3;
+  int caller = m_in.m1_i1;
+  int contender = m_in.m1_i2;
+  int scale = m_in.m1_i3;
 
   printf("Hello world!\n");
   return 0; // OK;
