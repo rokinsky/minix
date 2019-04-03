2aa26bfd145ee965ea703fa6837aabd0d3af9694
rokinsky <ankezy@gmail.com>
2019-04-03 02:00:21 +0200

    print test

--- root/test-distort_time.c
+++ root/test-distort_time.c
@@ -7,12 +7,7 @@ int main(int argc, char** argv)
 {
 	int res = distort_time(getpid(), 2);
 
-	if (res == OK)
-		printf("OK\n");
-	else if (res == EINVAL)
-		printf("EINVAL\n");
-	else if (res == EPERM)
-		printf("EPERM\n");
+	printf("%d\n", res);
 
 	return 0;
 }
