823f0a4400937b73e9f7420747805fb83ba0c378
rokinsky <ankezy@gmail.com>
2019-04-25 15:14:55 +0200

    fix

--- usr/src/lib/libc/misc/distort_time.c
+++ usr/src/lib/libc/misc/distort_time.c
@@ -7,14 +7,10 @@
 
 int distort_time(pid_t pid, uint8_t scale)
 {
-  message m = { .m1_i1 = pid, .m1_i2 = scale };
+  message m = { .m1_i1 = pid, .m1_i2 = scale, .m_type = PM_DISTORT_TIME };
 
-  // m->m_type = PM_DISTORT_TIME;
-  // int status = ipc_sendrec(PM_PROC_NR, &m);
-  // if (status != 0) return(status);
-  // return(msgptr->m_type);
-
-  return (_taskcall(PM_PROC_NR, PM_DISTORT_TIME, &m));
-
-  // return (_syscall(PM_PROC_NR, PM_DISTORT_TIME, &m)) == 0 ? 0 : errno;
+  /* I cannot include _taskcall from <minix/syslib.h> */
+  int status = ipc_sendrec(PM_PROC_NR, &m);
+  if (status != 0) return (status);
+  return (msgptr->m_type);
 }
