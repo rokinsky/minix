e5c6a9ce7b145da8449ff70f901af8ed8c0c95db
rokinsky <ankezy@gmail.com>
2019-04-25 15:23:47 +0200

    added comment

--- usr/src/lib/libc/misc/distort_time.c
+++ usr/src/lib/libc/misc/distort_time.c
@@ -1,15 +1,15 @@
-#include <errno.h>
 #include <inttypes.h>
 #include <lib.h>
-#include <minix/com.h>
 #include <unistd.h>
-#include <minix/syslib.h>
 
 int distort_time(pid_t pid, uint8_t scale)
 {
   message m = { .m1_i1 = pid, .m1_i2 = scale, .m_type = PM_DISTORT_TIME };
 
-  /* I cannot include _taskcall from <minix/syslib.h> */
+  /*
+   * I get undefined reference to `_taskcall'
+   * if I try to use `_taskcall` from <minix/syslib.h>.
+   */
   int status = ipc_sendrec(PM_PROC_NR, &m);
   if (status != 0) return (status);
   return (m.m_type);
