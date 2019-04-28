bd522e4eb1776d0d48a4460020cbbab9d54bcbcf
rokinsky <ankezy@gmail.com>
2019-04-28 19:42:55 +0200

    added _distortcall

--- usr/src/lib/libc/misc/distort_time.c
+++ usr/src/lib/libc/misc/distort_time.c
@@ -2,11 +2,20 @@
 #include <lib.h>
 #include <unistd.h>
 
+static int _distortcall(message *m)
+{
+  m->m_type = PM_DISTORT_TIME;
+
+  int status = ipc_sendrec(PM_PROC_NR, m);
+
+  if (status != 0) m->m_type = status;
+
+  return m->m_type < 0 ? -m->m_type : m->m_type;
+}
+
 int distort_time(pid_t pid, uint8_t scale)
 {
-  message m = { .m1_i1 = pid, .m1_i2 = scale, .m_type = PM_DISTORT_TIME };
+  message m = { .m1_i1 = pid, .m1_i2 = scale };
 
-  int status = ipc_sendrec(PM_PROC_NR, &m);
-  if (status != 0) return status;
-  return -m.m_type;
+  return _distortcall(&m);
 }
