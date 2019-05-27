b78116ba45a164bf09755a27ce37cc6fdfd214ce
rokinsky <ankezy@gmail.com>
2019-05-27 16:35:02 +0300

    bug

--- usr/src/minix/kernel/proc.h
+++ usr/src/minix/kernel/proc.h
@@ -26,7 +26,7 @@ struct proc {
   struct priv *p_priv;		/* system privileges structure */
   volatile u32_t p_rts_flags;	/* process is runnable only if zero */
   volatile u32_t p_misc_flags;	/* flags that do not suspend the process */
-  int p_picked;		/* is proccess was picked by proc_pick */
+  uint8_t p_picked;		/* is proccess was picked by proc_pick */
 
   char p_priority;		/* current process priority */
   u64_t p_cpu_time_left;	/* time left to use the cpu */
